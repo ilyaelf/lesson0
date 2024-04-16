3a8053526f12890feae8be9083d2678baa14e4d2
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-04-16 06:00:27 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,11 @@
+my_list = ['apple','pear','lemon','orange','pineapple','peach','banana']
+print('list:',my_list)
+print('first',my_list[0],'last',my_list[-1])
+print('3-5',my_list[2:5])
+my_list[3]='kiwi'
+print('new list' , my_list)
+my_dict = {'apple':'яблоко','pear':'груша','lemon':'лимон','orange':'апельсин','pineapple':'ананас','peach':'персик','banana':'банан'}
+print("slovar':",my_dict)
+print('pear - perevod:',my_dict['pear'])
+my_dict['kiwi'] = 'киви'
+print("izmenenniy slovar'",my_dict)
