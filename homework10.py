c119b8792e381c402388a9ffa0da042e49e2b58f
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-04-25 17:49:04 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,14 @@
+def def_test1():
+    a1 = 100500
+    b1 = 42
+    print('a1=',a1)
+    print('b1=',b1)
+
+def def_test2(a2,b2,c2):
+    print("a2=",a2,)
+    print('b2=',b2)
+    print('c2=',c2)
+
+
+def_test1()
+def_test2("100",100,5.5)
