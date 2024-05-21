a9f34695b65f3938abcf08d8e9eeebd6c6746fc4
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-05-21 15:16:32 +0300

    Add files via upload

---
+++ homework17.py
@@ -0,0 +1,12 @@
+class House:
+    def __init__(self,NumberOfFloors):
+        NumberOfFloors = 0
+
+    def setNewNumberOfFloors(floors):
+        NumberOfFloors = floors
+        print('количество этажей в доме:',NumberOfFloors,)
+
+
+House.setNewNumberOfFloors(6)
+ten_floor_house = House
+ten_floor_house.setNewNumberOfFloors(10)
