c98d048525db0bc707924a8350bc006a66151dcb
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-05-24 15:26:37 +0300

    Add files via upload

---
+++ homework19.py
@@ -0,0 +1,20 @@
+class Building:
+    total = 0
+    name = ""
+    def __init__(self,i):
+        self.name = 'building_№'+str(i+1)
+        self.total = 1+i
+        Building.total = Building.total + 1
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+for i in range(40):
+    building = Building(i)
+    print(building)
+
+print('building count',Building.total)
+
+
+
