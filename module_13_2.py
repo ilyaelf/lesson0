bb7e2ec4812603ad6e2e908b70073da8e0701840
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-08-24 17:43:21 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command, BaseFilter
+import asyncio
+import logging
+from aiogram.enums.dice_emoji import DiceEmoji
+
+logging.basicConfig(level=logging.DEBUG, filemode='w', filename='log.log',
+                    format="%(asctime)s|%(funcName)s|%(levelname)s|%(message)s|%(pathname)s")
+
+
+bot = Bot(token='')
+dp = Dispatcher()
+
+
+@dp.message(Command('start'))
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message()
+async def all_message(message: types.Message):
+    await message.answer('чтобы начать общение, введите /start')
+
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji=DiceEmoji.DICE)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    logging.basicConfig(level=logging.DEBUG, filemode='w', filename='log.log',
+                        format="%(asctime)s|%(funcName)s|%(levelname)s|%(message)s|%(pathname)s")
