561c828635b7413ae863fed390dba94ae4c1df14
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-09-17 13:27:19 +0300

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+connection = sqlite3.connect('Products.db')
+cursor = connection.cursor()
+
+
+def initiate_db():
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Products(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        description TEXT NOT NULL,
+        price INTEGER NOT NULL,
+        picture TEXT NOT NULL
+    )
+    ''')
+    connection.commit()
+
+
+def add_product(title, description, price, picture):
+    cursor.execute(' INSERT INTO Products (title,description,price,picture) VALUES (?,?,?,?)',
+                   (title, description, price, picture))
+    connection.commit()
+
+
+def get_all_products():
+    cursor.execute('SELECT * FROM Products')
+    products = cursor.fetchall()
+    connection.commit()
+    return products
+
+'''
+initiate_db()
+add_product('Продукт1', 'Витамин А', 100, 'pic1.png')
+add_product('Продукт2', 'Витамин B', 200, 'pic2.png')
+add_product('Продукт3', 'Витамин C', 300, 'pic3.png')
+add_product('Продукт4', 'Витамин D', 400, 'pic4.png')
+'''
+
