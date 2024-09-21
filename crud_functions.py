198b1cb1cb74944cc1c4111a902e6a78e0c4ab33
ilyaelf <166183350+ilyaelf@users.noreply.github.com>
2024-09-21 12:23:32 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -15,6 +15,15 @@ def initiate_db():
     ''')
     connection.commit()
 
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Users(
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+    )
+    ''')
+    connection.commit()
 
 def add_product(title, description, price, picture):
     cursor.execute(' INSERT INTO Products (title,description,price,picture) VALUES (?,?,?,?)',
@@ -28,6 +37,19 @@ def get_all_products():
     connection.commit()
     return products
 
+def add_user(username,email,age,balance):
+    cursor.execute('INSERT INTO Users (username,email,age,balance) VALUES (?,?,?,?)',
+                   (username,email,age,balance))
+    connection.commit()
+
+def is_included(username):
+    check_user=cursor.execute("SELECT * FROM Users WHERE username=?",(username,))
+    if check_user.fetchone() is None:
+        return False
+    else:
+        return True
+    connection.commit()
+
 '''
 initiate_db()
 add_product('Продукт1', 'Витамин А', 100, 'pic1.png')
@@ -35,4 +57,3 @@ add_product('Продукт2', 'Витамин B', 200, 'pic2.png')
 add_product('Продукт3', 'Витамин C', 300, 'pic3.png')
 add_product('Продукт4', 'Витамин D', 400, 'pic4.png')
 '''
-
